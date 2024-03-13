bcceacec61a18339157481f0ad40554d07fa5fcf
Lucas Matricarde <lucasmatricarde@hotmail.com>
2024-03-12 22:19:33 -0300

    Editar Pessoa

--- src/main/java/curso/sprinboot/springboot/controller/PessoaController.java
+++ src/main/java/curso/sprinboot/springboot/controller/PessoaController.java
@@ -1,7 +1,11 @@
 package curso.sprinboot.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,17 +20,19 @@ public class PessoaController {
 	private PessoaRepository pessoaRep;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastroPessoa")
-	public String inicio() {
-		return "cadastro/cadastroPessoa";
+	public ModelAndView inicio() {
+		ModelAndView view = new ModelAndView("cadastro/cadastroPessoa");
+		view.addObject("pessoaobj", new Pessoa());
+		return view;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/salvarPessoa")
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarPessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRep.save(pessoa);
-		
 		ModelAndView view = new ModelAndView("cadastro/cadastroPessoa");
 		Iterable<Pessoa> pessoasIt = pessoaRep.findAll();
 		view.addObject("pessoas", pessoasIt);
+		view.addObject("pessoaobj", new Pessoa());
 		return view;
 	}
 	
@@ -35,6 +41,15 @@ public class PessoaController {
 		ModelAndView view = new ModelAndView("cadastro/cadastroPessoa");
 		Iterable<Pessoa> pessoasIt = pessoaRep.findAll();
 		view.addObject("pessoas", pessoasIt);
+		view.addObject("pessoaobj", new Pessoa());
+		return view;
+	}
+	
+	@GetMapping("/editarPessoa/{idPessoa}")
+	public ModelAndView editar(@PathVariable("idPessoa") Long idPessoa) {
+		ModelAndView view = new ModelAndView("cadastro/cadastroPessoa");
+		Optional<Pessoa> pessoa = pessoaRep.findById(idPessoa);
+		view.addObject("pessoaobj", pessoa.get());
 		return view;
 	}
 }
