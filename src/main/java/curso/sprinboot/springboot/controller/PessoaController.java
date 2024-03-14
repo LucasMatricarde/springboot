eb5e7b954c0ca24e3873e1cb866fb695ae66c381
Lucas Matricarde <lucasmatricarde@hotmail.com>
2024-03-14 00:25:37 -0300

    Alterando estilo dos botoes

--- src/main/java/curso/sprinboot/springboot/controller/PessoaController.java
+++ src/main/java/curso/sprinboot/springboot/controller/PessoaController.java
@@ -4,10 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.sprinboot.springboot.model.Pessoa;
@@ -26,7 +23,7 @@ public class PessoaController {
 		return view;
 	}
 
-	@GetMapping("**/salvarPessoa")
+	@PostMapping ("**/salvarPessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRep.save(pessoa);
 		ModelAndView view = new ModelAndView("cadastro/cadastroPessoa");
